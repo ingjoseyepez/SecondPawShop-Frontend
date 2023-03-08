83f44ffec9e16ac6aa59892ec8296c56c9dc4056
AndresSArias <andres31321@gmail.com>
2023-03-08 02:35:01 -0500

    Create branch Users

---
+++ js/newProduct.js
@@ -0,0 +1,23 @@
+class Articulo {
+    constructor(nombre, categoria, descripcion, cantidad, imagenes) {
+      this.nombre = nombre;
+      this.categoria = categoria;
+      this.descripcion = descripcion;
+      this.cantidad = cantidad;
+      this.imagenes = imagenes;
+    }
+  }
+  
+  const formulario = document.querySelector('form');
+  formulario.addEventListener('submit', (event) => {
+    event.preventDefault(); // Prevenir que el formulario se envíe automáticamente
+  
+    const nombre = formulario.elements['nombre-articulo'].value;
+    const categoria = formulario.elements['categoria'].value;
+    const descripcion = formulario.elements['descripcion-producto'].value;
+    const cantidad = formulario.elements['cantidad'].value;
+    const imagenes = formulario.elements['imagenes'].files;
+  
+    const articulo = new Articulo(nombre, categoria, descripcion, cantidad, imagenes);
+    console.log(articulo);
+  });
