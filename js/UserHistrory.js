4e51fe549485e55897688f617b39adfa2241f084
AndresSArias <andres31321@gmail.com>
2023-04-23 12:42:34 -0500

    [Update] endpoint of History User

--- js/UserHistrory.js
+++ js/UserHistrory.js
@@ -1,5 +1,5 @@
 const userData = JSON.parse(localStorage.getItem("userData"));
-const id = userData.idUsuario;
+const id = userData.id;
 const url = "http://localhost:8080/Usuario/Historial/" + id;
 const HTMLResponse = document.getElementById("historial");
 
