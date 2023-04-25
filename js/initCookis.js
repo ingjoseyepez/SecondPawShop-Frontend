b45e5bc5114a5655fd7a09263935c55d27dcec4a
jose Yepez <ing.joseyepez@gmail.com>
2023-04-25 01:00:32 -0500

    [udapte] proyecto

--- js/initCookis.js
+++ js/initCookis.js
@@ -1,11 +1,9 @@
 //Funciones
-function iniciarlizarIsLogin (){
-    if (!isInitialized) {
-      // Inicializar el localStorage y guardar la cookie "isInitialized"
-      localStorage.setItem('isLoggedIn', false);
-      document.cookie = 'isInitialized=true; expires=Fri, 31 Dec 9999 23:59:59 GMT; path=/';
-    }
-}
+
 // Obtener el valor de la cookie "isInitialized"
 const isInitialized = document.cookie.split(';').some((item) => item.trim().startsWith('isInitialized='));
-iniciarlizarIsLogin();
+if (!isInitialized) {
+  // Inicializar el localStorage y guardar la cookie "isInitialized"
+  window.localStorage.setItem('isLoggedIn', false);
+  document.cookie = 'isInitialized=true; expires=Fri, 31 Dec 9999 23:59:59 GMT; path=/';
+}
